83c7dd2fb716eb7a36ee9843a18ddfcc442c38b7
Ra3L <85597883+Ra3L@users.noreply.github.com>
2023-10-23 17:51:12 +1100

    removed toaster

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { Toaster } from "react-hot-toast";
-
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
@@ -16,24 +14,6 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
-      <Toaster
-        position="top-right"
-        gutter={10}
-        containerStyle={{ margin: "6px" }}
-        toastOptions={{
-          success: {
-            duration: 3000,
-          },
-          error: { duration: 5000 },
-          style: {
-            fontSize: "16px",
-            maxWidth: "400px",
-            padding: "8px 12px",
-            backgroundColor: "#d7d7d7",
-            color: "#393939",
-          },
-        }}
-      />
     </>
   );
 }
